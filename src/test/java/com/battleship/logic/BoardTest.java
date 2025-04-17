d476670a38b0925a6eba78d88c89e2d7781df002
Aaron Basil Serro <aserro2@uwo.ca>
2025-04-17 16:27:30 -0400

    project deployemtn config

--- src/test/java/com/battleship/logic/BoardTest.java
+++ src/test/java/com/battleship/logic/BoardTest.java
@@ -1,5 +1,22 @@
 package com.battleship.logic;
 
+import org.junit.jupiter.api.Test;
+
 public class BoardTest {
+    @Test
+    void testFireonHit(){
+
+    }
+    @Test
+    void testFireonInvalid(){}
+
+    @Test
+    void testFireonMiss(){}
+
+    @Test
+    void testFireonSunk(){}
+
+
+
 
 }
---
+++ src/test/java/com/battleship/logic/ShipTest.java
@@ -0,0 +1,19 @@
+package com.battleship.logic;
+
+import org.junit.jupiter.api.Test;
+
+public class ShipTest {
+    @Test
+    void testlength(){}
+
+    @Test
+    void testCoordinates(){}
+
+    @Test
+    void testsunk(){}
+
+    @Test
+    void registerHit(){}
+
+
+}
